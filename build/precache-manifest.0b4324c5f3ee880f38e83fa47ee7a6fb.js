00476c0531ad3acbeafe2588724574e72b4a1135
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-04-24 20:15:03 +0200

    change

---
+++ build/precache-manifest.0b4324c5f3ee880f38e83fa47ee7a6fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a9c7de0ebfe2f54a9792f0700be45c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b3ce34d685ef20dcd9",
+    "url": "/static/css/main.221589fe.chunk.css"
+  },
+  {
+    "revision": "955203cebaf338d06d3d",
+    "url": "/static/js/2.05c6293c.chunk.js"
+  },
+  {
+    "revision": "11b3ce34d685ef20dcd9",
+    "url": "/static/js/main.acb32680.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
